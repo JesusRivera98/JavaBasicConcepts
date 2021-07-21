7cc6c8fa80c48c9020434017132e3ce5e3b01558
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-21 17:10:24 -0500

    Number class

--- Test.java
+++ Test.java
@@ -1,18 +1,8 @@
 public class Test {
 
    public static void main(String args[]) {
-      int [] numbers = {10, 20, 30, 40, 50};
-
-      for(int x : numbers ) {
-         System.out.print( x );
-         System.out.print(",");
-      }
-      System.out.print("\n");
-      String [] names = {"James", "Larry", "Tom", "Lacy"};
-
-      for( String name : names ) {
-         System.out.print( name );
-         System.out.print(",");
-      }
+      Integer x = 5; // boxes int to an Integer object
+      x =  x + 10;   // unboxes the Integer to a int
+      System.out.println(x); 
    }
 }
