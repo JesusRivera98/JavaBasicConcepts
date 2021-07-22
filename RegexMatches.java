ba16355e9ae875a1f5c81746bf9dad6539c99cff
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 11:53:44 -0500

    Regular Expressions- appendReplacement & appendTail

--- RegexMatches.java
+++ RegexMatches.java
@@ -3,16 +3,20 @@ import java.util.regex.Pattern;
 
 public class RegexMatches {
 
-   private static String REGEX = "dog";
-   private static String INPUT = "The dog says meow. " + "All dogs say meow.";
-   private static String REPLACE = "cat";
-
+   private static String REGEX = "a*b";
+   private static String INPUT = "aabfooaabfooabfoob";
+   private static String REPLACE = "-";
    public static void main(String[] args) {
+
       Pattern p = Pattern.compile(REGEX);
       
       // get a matcher object
-      Matcher m = p.matcher(INPUT); 
-      INPUT = m.replaceAll(REPLACE);
-      System.out.println(INPUT);
+      Matcher m = p.matcher(INPUT);
+      StringBuffer sb = new StringBuffer();
+      while(m.find()) {
+         m.appendReplacement(sb, REPLACE);
+      }
+      m.appendTail(sb);
+      System.out.println(sb.toString());
    }
 }
