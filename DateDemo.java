86c7529644d36e29402afc4caaa7d10aa3c5e992
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 10:29:28 -0500

    Date & Time - formatting printf II

--- DateDemo.java
+++ DateDemo.java
@@ -4,10 +4,8 @@ public class DateDemo {
    public static void main(String args[]) {
       // Instantiate a Date object
       Date date = new Date();
-
+  
       // display time and date
-      String str = String.format("Current Date/Time : %tc", date );
-
-      System.out.printf(str);
+      System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
    }
 }
