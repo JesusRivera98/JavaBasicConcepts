c52efe25653127787b0c4ce2889b50d4afc194b0
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 10:32:11 -0500

    Date & Time - formatting printf III

--- DateDemo.java
+++ DateDemo.java
@@ -5,7 +5,7 @@ public class DateDemo {
       // Instantiate a Date object
       Date date = new Date();
   
-      // display time and date
-      System.out.printf("%1$s %2$tB %2$td, %2$tY", "Due date:", date);
+      // display formatted date
+      System.out.printf("%s %tB %<te, %<tY", "Due date:", date);
    }
 }
