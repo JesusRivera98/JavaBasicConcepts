431c16f0582da05d9fc98a4ee988c971038b4da8
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 17:03:16 -0500

    Exercises: Correct names

--- Exercises/Exercise3.java
+++ Exercises/Exercise3.java
@@ -2,12 +2,12 @@ import java.io.*;
 
 public class Exercise3 {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String cadena;
-        FileReader f = new FileReader("input.txt");
-        BufferedReader b = new BufferedReader(f);
-        while((cadena = b.readLine())!=null) {
-            System.out.println(cadena);
+        String str;
+        FileReader file = new FileReader("input.txt");
+        BufferedReader buffer = new BufferedReader(file);
+        while((str = buffer.readLine())!=null) {
+            System.out.println(str);
         }
-        b.close();
+        buffer.close();
     }
 }
