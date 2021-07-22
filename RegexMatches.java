633002b544324b4a8f22bda8a6bde3172907086e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 11:14:53 -0500

    Regular Expressions- Sart & End

--- RegexMatches.java
+++ RegexMatches.java
@@ -3,22 +3,19 @@ import java.util.regex.Pattern;
 
 public class RegexMatches {
 
+   private static final String REGEX = "\\bcat\\b";
+   private static final String INPUT = "cat cat cat cattie cat";
+
    public static void main( String args[] ) {
-      // String to be scanned to find the pattern.
-      String line = "This order was placed for QT3000! OK?";
-      String pattern = "(.*)(\\d+)(.*)";
+      Pattern p = Pattern.compile(REGEX);
+      Matcher m = p.matcher(INPUT);   // get a matcher object
+      int count = 0;
 
-      // Create a Pattern object
-      Pattern r = Pattern.compile(pattern);
-
-      // Now create matcher object.
-      Matcher m = r.matcher(line);
-      if (m.find( )) {
-         System.out.println("Found value: " + m.group(0) );
-         System.out.println("Found value: " + m.group(1) );
-         System.out.println("Found value: " + m.group(2) );
-      }else {
-         System.out.println("NO MATCH");
+      while(m.find()) {
+         count++;
+         System.out.println("Match number "+count);
+         System.out.println("start(): "+m.start());
+         System.out.println("end(): "+m.end());
       }
    }
 }
