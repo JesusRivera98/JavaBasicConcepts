57c946237028b5081fb61f2cdd0f38a3edde0614
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 10:26:07 -0500

    Date & Time - formatting

--- DateDemo.java
+++ DateDemo.java
@@ -1,11 +1,13 @@
-import java.util.Date;
+import java.util.*;
+import java.text.*;
+
 public class DateDemo {
 
    public static void main(String args[]) {
-      // Instantiate a Date object
-      Date date = new Date();
+      Date dNow = new Date( );
+      SimpleDateFormat ft = 
+      new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
 
-      // display time and date using toString()
-      System.out.println(date.toString());
+      System.out.println("Current Date: " + ft.format(dNow));
    }
 }
