745dbd5f8173cafbb772eb4da18d6e6ac8e5891e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 10:34:19 -0500

    Date & Time - Stings into dates

--- DateDemo.java
+++ DateDemo.java
@@ -1,11 +1,19 @@
-import java.util.Date;
+import java.util.*;
+import java.text.*;
+  
 public class DateDemo {
 
    public static void main(String args[]) {
-      // Instantiate a Date object
-      Date date = new Date();
-  
-      // display formatted date
-      System.out.printf("%s %tB %<te, %<tY", "Due date:", date);
+      SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd"); 
+      String input = args.length == 0 ? "1818-11-11" : args[0]; 
+
+      System.out.print(input + " Parses as "); 
+      Date t;
+      try {
+         t = ft.parse(input); 
+         System.out.println(t); 
+      } catch (ParseException e) { 
+         System.out.println("Unparseable using " + ft); 
+      }
    }
 }
