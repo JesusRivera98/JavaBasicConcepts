d71d5f5b457189ad1bcb09e18bb4fd4428acf754
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 14:19:44 -0500

    Inheritance: IS-A Relationship

--- Dog.java
+++ Dog.java
@@ -1,14 +1,21 @@
-public class Dog {
-   String breed;
-   int age;
-   String color;
+class Animal {
+}
 
-   void barking() {
-   }
+class Mammal extends Animal {
+}
 
-   void hungry() {
-   }
+class Reptile extends Animal {
+}
 
-   void sleeping() {
+public class Dog extends Mammal {
+
+   public static void main(String args[]) {
+      Animal a = new Animal();
+      Mammal m = new Mammal();
+      Dog d = new Dog();
+
+      System.out.println(m instanceof Animal);
+      System.out.println(d instanceof Mammal);
+      System.out.println(d instanceof Animal);
    }
 }
