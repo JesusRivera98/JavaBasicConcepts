5adef86aca839937f01ff299e4787926128e7814
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-22 11:50:45 -0500

    Regular Expressions- replaceFirst & replaceAll

--- RegexMatches.java
+++ RegexMatches.java
@@ -3,19 +3,16 @@ import java.util.regex.Pattern;
 
 public class RegexMatches {
 
-   private static final String REGEX = "foo";
-   private static final String INPUT = "fooooooooooooooooo";
-   private static Pattern pattern;
-   private static Matcher matcher;
+   private static String REGEX = "dog";
+   private static String INPUT = "The dog says meow. " + "All dogs say meow.";
+   private static String REPLACE = "cat";
 
-   public static void main( String args[] ) {
-      pattern = Pattern.compile(REGEX);
-      matcher = pattern.matcher(INPUT);
-
-      System.out.println("Current REGEX is: "+REGEX);
-      System.out.println("Current INPUT is: "+INPUT);
-
-      System.out.println("lookingAt(): "+matcher.lookingAt());
-      System.out.println("matches(): "+matcher.matches());
+   public static void main(String[] args) {
+      Pattern p = Pattern.compile(REGEX);
+      
+      // get a matcher object
+      Matcher m = p.matcher(INPUT); 
+      INPUT = m.replaceAll(REPLACE);
+      System.out.println(INPUT);
    }
 }
